9c9b3b3d2afc618b1128f3cb12cd1243f25ab9ca
Pavel Viznitsa <viznitsap@gmail.com>
2017-05-27 17:37:04 +0300

    add banner-slider and dots

--- js/main.js
+++ js/main.js
@@ -3,5 +3,27 @@ $(document).ready(function() {
     $("li").click(function () {
         $("li").removeClass("active");
         $(this).addClass("active");
+    });
+// });
+
+// $(document).ready(function() {
+    var images = ['work-1.jpg', 'work-2.jpg', 'work-3.jpg', 'work-4.jpg', 'work-5.jpg'];
+    var currImg = 0;
+
+    $('.imgBanner').css('background-image', 'url(img/' + images[currImg] + ')');
+
+    for (var i = 0; i < images.length; i++) {
+        $('.dots').append('<div data-img="' + i + '" class="dot">');
+    }
+
+    $('.dot:first').addClass('active-dot');
+
+    $('.dot').click(function() {
+        $('.dot').removeClass('active-dot');
+        $(this).addClass('active-dot');
+        currImg = $(this).attr('data-img');
+        $('.imgBanner').css('background-image', 'url(img/' + images[currImg] + ')');
     })
+
 });
+
